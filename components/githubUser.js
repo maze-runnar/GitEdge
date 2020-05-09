a2dcd422fb0018f8ad7ed69076295ad8c0889119
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-05-09 19:15:29 +0530

    fetching repos

--- components/githubUser.js
+++ components/githubUser.js
@@ -10,12 +10,12 @@ import {
   GitHubSocialButton, LinkedInSocialButton, MicrosoftSocialButton } from "react-native-social-buttons";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Card, WhiteSpace, WingBlank } from '@ant-design/react-native';
+import {  WhiteSpace, WingBlank ,Card} from '@ant-design/react-native';
 //import Share from 'react-native-share';
 import { toast } from 'react-toastify';
 import { Icon } from 'react-native-elements';
 //import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input } from 'react-native-elements';
+import {Divider, ListItem, Input } from 'react-native-elements';
 //import { Button } from 'react-native-elements';
 import { TextInput, Button , Image, Linking, Switch} from 'react-native';
 
@@ -64,13 +64,34 @@ const styles = StyleSheet.create({ bgContainer: { flex:1, width: null, height: n
  	height: 50, 
  	fontSize:20,
  	color : "white"
+ 	},
+
+ 	maincontent:{
+ 		backgroundColor:'green',
+ 		 elevation: 5,
+ 		 margin: 10, 
+ 		borderRadius:5,
+ 		 width:200,
+ 		 height: 100, 
+ 		textAlign: 'center',
+ 		 justifyContent: 'center',
+ 		 padding:5
  	}
 
 
 
-
-
     });
+
+
+
+const users = [
+ {
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+ },
+]
+
+
 
 
 export default class movielist extends React.Component {
@@ -80,6 +101,7 @@ constructor(props) {
     this.state = {username: 'maze-runnar',
     				login: '',
     				name:'',
+    				bio:'',
     				avatar_url: '',
     				url:'',
     				html_url: '',
@@ -98,7 +120,8 @@ constructor(props) {
     				created_at:'',
     				updated_at:'', 
     				show : false, 
-    				darkmode: true
+    				darkmode:false,
+    				user:[]
     				};
 
     //this.handleChange = this.handleChange.bind(this);
@@ -115,6 +138,7 @@ handleSubmit(){
 				      	avatar_url: res.avatar_url,
 				      	url: res.url,
 				      	html_url: res.html_url,
+				      	bio: res.bio,
 				      	followers_url: res.followers_url,
 				      	repos_url: res.repos_url,
 				      	company: res.company,
@@ -127,6 +151,17 @@ handleSubmit(){
 				      	show : true
 				      })
 				    })
+
+				    .then(fetch(`https://api.github.com/users/${this.state.username}/repos`)
+				    	.then((response) => response.json())
+				    	.then((res) => {
+				    		return this.setState({
+				    			user : res
+				    		})
+				    	}
+				    	)
+				    )
+
 				    .catch((error) => {
 				      console.error(error);
 				    });
@@ -163,9 +198,16 @@ toggleSwitch(){
 			      onChangeText={(username) => this.setState({username})}
 			      value={this.state.username}
 			    />
-			<View style ={{padding: 10, width:150, marginLeft:80}}>
-				<Button onPress={this.handleSubmit} title="Search User" color="red" />
+
+			    {this.state.username?
+
+			   <View style ={{padding: 10, width:150, marginLeft:80}}>
+				<Button onPress={this.handleSubmit} title="Search User" color="green" />
 			</View>
+					:
+					<Text></Text>
+			    }
+			
 			
 			
 			 <Image
@@ -179,6 +221,8 @@ toggleSwitch(){
 
 		      <View style={{textAlign: 'center'}}>
 		      <Text style={{fontSize:20, textAlign: 'center'}}> {this.state.name} </Text>
+		     <Text style={{fontSize:10, textAlign: 'center'}}> {this.state.bio} </Text>
+
 		      </View>
 
 
@@ -186,40 +230,65 @@ toggleSwitch(){
 
 
 		      	<ScrollView horizontal = {true}>
-		      <View style={{backgroundColor:'purple', elevation: 5, margin: 10, borderRadius:5, width:200, height: 200, textAlign: 'center', justifyContent: 'center'}}>
-		      <Text style={{fontSize: 20}}>Go to Github: </Text>
-			    <Text style={{color: 'white', fontSize: 20}}
+		      <View style={styles.maincontent}>
+		     
+			    <Text style={{color: 'white', fontSize: 15}}
 	      			onPress={() => Linking.openURL(this.state.html_url)}>{this.state.html_url}</Text>
 	      		</View>
-      		<View style={{backgroundColor:'purple', elevation: 5, margin: 10, borderRadius:5, width:200, height: 200, textAlign: 'center', justifyContent: 'center'}}>
-		      <Text style={{fontSize:20, color: 'white'}}> Api : {this.state.url}</Text>
+      		
+			<View style={styles.maincontent}>
+		      <Text style={{fontSize:15, color: 'white'}}> Following : {this.state.following}</Text>
 			</View>
-			<View style={{backgroundColor:'purple', elevation: 5, margin: 10, borderRadius:5, width:200, height: 200, textAlign: 'center', justifyContent: 'center'}}>
-		      <Text style={{fontSize:20, color: 'white'}}> Following : {this.state.following}</Text>
+			<View style={styles.maincontent}>
+		      <Text style={{fontSize:15, color: 'white'}}> Followerss : {this.state.followers}</Text>
 			</View>
-			<View style={{backgroundColor:'purple', elevation: 5, margin: 10, borderRadius:5, width:200, height: 200, textAlign: 'center', justifyContent: 'center'}}>
-		      <Text style={{fontSize:20, color: 'white'}}> Followerss : {this.state.followers}</Text>
+			<View style={styles.maincontent}>
+		      <Text style={{fontSize:15, color: 'white'}}> Blog : {this.state.blog}</Text>
 			</View>
-			<View style={{backgroundColor:'purple', elevation: 5, margin: 10, borderRadius:5, width:200, height: 200, textAlign: 'center', justifyContent: 'center'}}>
-		      <Text style={{fontSize:20, color: 'white'}}> Blog : {this.state.blog}</Text>
+			<View style={styles.maincontent}>
+		      <Text style={{fontSize:15, color: 'white'}}> Company : {this.state.company}</Text>
 			</View>
-			<View style={{backgroundColor:'purple', elevation: 5, margin: 10, borderRadius:5, width:200, height: 200, textAlign: 'center', justifyContent: 'center'}}>
-		      <Text style={{fontSize:20, color: 'white'}}> Company : {this.state.company}</Text>
+			<View style={styles.maincontent}>
+		      <Text style={{fontSize:15, color: 'white'}}> Email : {this.state.email}</Text>
 			</View>
-			<View style={{backgroundColor:'purple', elevation: 5, margin: 10, borderRadius:5, width:200, height: 200, textAlign: 'center', justifyContent: 'center'}}>
-		      <Text style={{fontSize:20, color: 'white'}}> Email : {this.state.email}</Text>
+			<View style={styles.maincontent}>
+		      <Text style={{fontSize:15, color: 'white'}}> created_at : {this.state.created_at}</Text>
+			
 			</View>
-			<View style={{backgroundColor:'purple', elevation: 5, margin: 10, borderRadius:5, width:200, height: 200, textAlign: 'center', justifyContent: 'center'}}>
-		      <Text style={{fontSize:20, color: 'white'}}> created_at : {this.state.created_at}</Text>
-			</View>
-			</ScrollView>
 
+			</ScrollView>
 
 
 		       :<Text style ={{marginLeft:100}}> nothing to show</Text>}
 
+		       
+				  {
+				    this.state.user.map((u, i) => {
+				      return (
+				      <View style={{marginLeft:10, paddingBottom:5 , paddingTop: 5, marginBottom:5}}>
+				      <Card style={{elevation: 4, width:300}}>
 
-		      
+				      <Card.Header
+			              title={u.name}
+			              thumbStyle={{ width: 30, height: 40 }}
+			              thumb={u.owner["avatar_url"]}
+			        
+			            />
+			            <Card.Body>
+			              <View>
+			                <Text style={{ marginLeft: 16 }}>{u.description ? u.description : <Text>No description available</Text>}</Text>
+			              </View>
+			            </Card.Body>
+			            <Card.Footer
+			              content={u.html_url}
+			          
+			            />
+				       </Card>
+				       </View>
+				      );      
+				    })
+				  }
+				
 			</ScrollView>
 			
   		</View>
